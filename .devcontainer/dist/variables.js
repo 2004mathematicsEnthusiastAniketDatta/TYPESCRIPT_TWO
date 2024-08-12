4ed843f4e3a3a275bceab86080bcfba9fec7d2c6
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-08-12 17:06:55 +0000

    variables

---
+++ .devcontainer/dist/variables.js
@@ -0,0 +1,16 @@
+let a = 5.12;
+console.log(a);
+const accountId = 144553;
+let accountEmail = "aniket@protonmail.com";
+let accountPassword = "12345";
+let accountCity = "Kolkata";
+// accountId = 5.12;
+accountEmail = "aniketdatta@protonmail.com";
+//console.table(accountId,accountEmail);
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+// variable: value storing field or container in memory
+console.table([accountId, accountEmail, accountCity]);
+ //var is not block-scoped
---
+++ .devcontainer/server/TYPESCRIPT/variables.ts
@@ -0,0 +1,22 @@
+import { Console } from "console";
+
+let a : Number =5.12;
+console.log(a);
+const accountId : Number =144553;
+let accountEmail : String ="aniket@protonmail.com";
+let accountPassword : String = "12345";
+let accountCity : String = "Kolkata";
+
+// accountId = 5.12;
+
+accountEmail = "aniketdatta@protonmail.com" 
+//console.table(accountId,accountEmail);
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+// variable: value storing field or container 
+ 
+console.table([accountId,accountEmail,accountCity])
