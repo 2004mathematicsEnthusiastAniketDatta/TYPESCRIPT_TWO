afef93b91d9dd7eeaf159614b203936bb8b3be70
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-08-12 16:05:20 +0000

    okay

---
+++ .devcontainer/dist/test.js
@@ -0,0 +1,10 @@
+"use strict";
+//import fs from "fs";
+console.log("TYPESCRIPT");
+const addNumber = (a, b) => {
+    return a + b;
+};
+console.log(addNumber(1, 2));
+console.log("okay");
+//fs.writeFileSync('test.txt','test');
+console.log("HELLO WORLD");
---
+++ .devcontainer/server/TYPESCRIPT/test.ts
@@ -0,0 +1,9 @@
+//import fs from "fs";
+console.log("TYPESCRIPT");
+const addNumber = (a: Number , b:Number) => {
+    return a + b
+}
+console.log(addNumber(1,2));
+console.log("okay");
+//fs.writeFileSync('test.txt','test');
+console.log("HELLO WORLD");
