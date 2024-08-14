500299ce867987aaacd7de0aceb2c9436839cd3b
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-08-14 17:54:18 +0000

    ok

--- .devcontainer/dist/Number_Maths_Javascript.js
+++ .devcontainer/dist/Number_Maths_Javascript.js
@@ -18,5 +18,5 @@ console.log(Math.floor(4.9));
 console.log(Math.min(4,3,6,8));
 console.log(Math.random());
 console.log(Math.random());
-console.log(Math.random()*10);
+console.log(Math.random()*10 + 1);
 
