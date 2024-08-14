af05911eb1b773166be89bc2644a50d8977408cc
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-08-14 17:32:13 +0000

    maths

--- .devcontainer/dist/Number_Maths_Javascript.js
+++ .devcontainer/dist/Number_Maths_Javascript.js
@@ -11,3 +11,7 @@ Number.MIN_VALUE;
 // +++++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++
 console.log(Math);
 console.log(Math.abs(-4));
+console.log(Math.abs(4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
