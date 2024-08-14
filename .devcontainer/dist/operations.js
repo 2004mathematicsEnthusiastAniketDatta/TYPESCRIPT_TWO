ee43ecc503101761dccc1196ec3a4c43ac8ef3f8
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-08-14 16:20:43 +0000

    console

---
+++ .devcontainer/dist/operations.js
@@ -0,0 +1,23 @@
+"use strict";
+let value = 3;
+let negValue = -value;
+// console.log(negValue) 
+console.log(negValue);
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 - 2);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log((3 + 4) * 5 % 3);
+console.log(+true);
+console.log(+"");
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+let gameCounter = 100;
+//gameCounter++;
+++ gameCounter;
+console.log(gameCounter);
---
+++ .devcontainer/server/TYPESCRIPT/operations.ts
@@ -0,0 +1,24 @@
+let value: Number = 3;
+let negValue: Number = -value;
+// console.log(negValue) 
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2-2);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2);
+console.log("1"+2+2);
+console.log(1 + 2 + "2");
+console.log((3 + 4 ) * 5 % 3);
+console.log(+true);
+console.log(+"");
+let num1 , num2 , num3;
+
+num1 = num2 = num3 = 2+2
+let gameCounter = 100;
+//gameCounter++;
+++ gameCounter;
+console.log(gameCounter);
