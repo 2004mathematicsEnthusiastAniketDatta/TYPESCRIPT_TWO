5355b63133c973cb262f753a951aa5d6cc601987
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-08-21 16:20:56 +0000

    console

--- .devcontainer/dist/datatypes.js
+++ .devcontainer/dist/datatypes.js
@@ -1,32 +1,26 @@
+"use strict";
 // Primitive Datatypes
 // 7 types: String , Number , Boolean , null , undefined , Symbol , BigInt
-
-const scr:number =100;
-const scrValue: number = 100.3;
+const scr = 100;
+const scrValue = 100.3;
 // const isloggedin: number = false;
 const outsidetemp = null;
 // let userEmail;
-const id: Symbol = Symbol('123');
-const anotherid: Symbol = Symbol('456');
-console.log(id === anotherid)
-
+const id = Symbol('123');
+const anotherid = Symbol('456');
+console.log(id === anotherid);
 // const bigNumber:BigInt = 3456543578665855n;
-
 //Non-Primitve Datatypes in C
-
 //Reference types: 
 // Arrays , Objects , Functions
-
-const  heroes = ["Shaktiman" , "naagraj" , "doga"];
+const heroes = ["Shaktiman", "naagraj", "doga"];
 let myObj = {
     name: "hitesh",
     age: 22,
-}
-
-const  myfunction = function(){
+};
+const myfunction = function () {
     console.log("Hello world");
-}
-
+};
 console.log(typeof heroes);
 console.log(typeof myObj);
 console.log(typeof myfunction);
